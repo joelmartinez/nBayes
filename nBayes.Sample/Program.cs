475789e85407f96d7b79060000960f9de05911ab
Joel Martinez <joelmartinez@gmail.com>
2012-05-31 12:34:09 -0400

    updated sample program to use the TPL in order to parallelize the twitter API queries. Requests now happen in the background while the user supplies the phrase to categorize.

--- nBayes.Sample/Program.cs
+++ nBayes.Sample/Program.cs
@@ -2,6 +2,11 @@
 using System.ServiceModel.Syndication;
 using System.Web;
 using System.Xml;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Text;
 
 namespace nBayes
 {
@@ -12,17 +17,21 @@ namespace nBayes
             // Create indexes
             Console.Write("First Index: ");
             string firstSearch = Console.ReadLine();
-            var first = CreateIndex(firstSearch);
+            var firsttask = CreateIndex(firstSearch);
 
             Console.Write("Second Index: ");
             string secondSearch = Console.ReadLine();
-            var second = CreateIndex(secondSearch);
+            var secondtask = CreateIndex(secondSearch);
 
             Analyzer analyzer = new Analyzer();
 
             Console.Write("Text to Categorize: ");
-
-            CategorizationResult result = analyzer.Categorize(Entry.FromString(Console.ReadLine()), first, second);
+			var entryToCategorize = Entry.FromString(Console.ReadLine());
+			
+			var first = firsttask.Result;
+			var second = secondtask.Result;
+			
+            CategorizationResult result = analyzer.Categorize(entryToCategorize, first, second);
 
             Console.Write("And the verdict is ... ");
 
@@ -43,21 +52,99 @@ namespace nBayes
             second.Save();
         }
 
-        private static FileIndex CreateIndex(string input)
+        private static Task<FileIndex> CreateIndex(string input)
         {
             var scrubbed = HttpUtility.UrlEncode(input);
-            var reader = XmlReader.Create(string.Format("http://search.twitter.com/search.atom?rpp=100&lang=en&q={0}", scrubbed));
-            var feed = SyndicationFeed.Load(reader);
-
-            var index = new FileIndex(input + ".xml");
-            index.Open();
-
-            foreach (SyndicationItem item in feed.Items)
-            {
-                index.Add(Entry.FromString(item.Title.Text));
-            }
-
-            return index;
+            var task = WebHelper.Feed(string.Format("http://search.twitter.com/search.atom?rpp=100&lang=en&q={0}", scrubbed))
+				.ContinueWith(r =>
+				{
+		            var feed = r.Result;
+		
+		            var index = new FileIndex(input + ".xml");
+		            index.Open();
+		
+		            foreach (SyndicationItem item in feed.Items)
+		            {
+		                index.Add(Entry.FromString(item.Title.Text));
+		            }
+		
+		            return index;
+				});
+			
+			return task;
         }
     }
+
+	/// <summary>simple web helper to simplify the web request</summary>
+	internal static class WebHelper
+	{
+		public static Task<string> Get(string url) {
+			var tcs = new TaskCompletionSource<string>();
+            var request = (HttpWebRequest)WebRequest.Create(url);
+			request.UserAgent = "nBayes sample client";
+            try
+            {
+                request.BeginGetResponse(iar =>
+                {
+                    HttpWebResponse response = null;
+                    try
+                    {
+                        response = (HttpWebResponse)request.EndGetResponse(iar);
+                        if (response.StatusCode == HttpStatusCode.OK)
+                        {
+                            var sreader = new StreamReader(response.GetResponseStream());
+                            var result = sreader.ReadToEnd();
+
+							tcs.SetResult(result);
+                        }
+                        else
+                        {
+                            tcs.SetResult(string.Empty);
+                        }
+                    }
+                    catch (Exception exc) { tcs.SetException(exc); }
+                    finally { if (response != null) response.Close(); }
+                }, null);
+            }
+            catch (Exception exc) { tcs.SetException(exc); }
+            return tcs.Task;
+		}
+		
+		public static Task<SyndicationFeed> Feed(string url) {
+			var tcs = new TaskCompletionSource<SyndicationFeed>();
+			Get (url).ContinueWith(result => 
+			{
+				TextReader txtreader = new StringReader(result.Result);
+				XmlReader reader = XmlReader.Create(txtreader);
+				var feed = SyndicationFeed.Load(reader);
+				tcs.SetResult(feed);
+			});
+			return tcs.Task;
+		}
+		
+		public static Task<T> Json<T>(string url)
+        {
+            var tcs = new TaskCompletionSource<T>();
+			
+			Get (url).ContinueWith(result =>
+            {
+				try
+				{
+	                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+	                byte[] bytes = Encoding.UTF8.GetBytes(result.Result);
+	                using (var stream = new MemoryStream(bytes))
+	                {
+	                    var deserialized = serializer.ReadObject(stream);
+	
+	                    tcs.SetResult( (T)deserialized );
+	                }
+				}
+                catch (Exception exc) { tcs.SetException(exc); }
+			});
+			
+			return tcs.Task;
+		}
+	}
 }
+
+
