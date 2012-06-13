2648564f7fdeb282b21103caa837f32ba58321db
Alexander D. Martinez <alekisan@gmail.com>
2012-06-12 22:03:30 -0400

    Added test project
    Also added the references. No actual code added yet.

---
+++ nBayes.test/TestnBayes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using nBayes;
+
+namespace nBayes.test
+{
+    [TestFixture]
+    public class TestnBayes
+    {
+    }
+}
